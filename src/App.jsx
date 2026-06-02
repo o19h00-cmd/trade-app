ee1ce48c86844e07df23375a61ee4b2f977848d6
o19h00-cmd <o19h00@gmail.com>
2026-06-03 02:52:57 +0900

    fix useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from "recharts";
